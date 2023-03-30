42176f98636ddb9517e577706a28dc04c68be68d
R0ttenr0b <124658302+R0ttenr0b@users.noreply.github.com>
2023-03-30 14:16:42 -0700

    starting adding the var and first draw loop

--- game.js
+++ game.js
@@ -5,4 +5,26 @@ var sprite = new Image();
 sprite.src = "0.png";
 sprite.onload = function() {
     ctx.drawImage(sprite, 0, 0);
+
+var penguinX =0;
+var penguinY =0;
+var penguinX2 =0;
+var penguinY2 =0;
+
+
+function drawspenguin(){
+  ctx clearRect(0,0,canvas.canvas.width, canvas.height);
+  ctx,drawImage(image,penguinX, penguinY)
+
+}
+
+
+
+
+
+
+
+
+
+
   };
