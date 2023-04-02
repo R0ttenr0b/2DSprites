bfecf7b8f1b77d61ef65f4a0b2784cf653baa9a9
R0ttenr0b <124658302+R0ttenr0b@users.noreply.github.com>
2023-04-01 20:39:42 -0700

    working with the penguins

--- game.js
+++ game.js
@@ -12,19 +12,30 @@ var penguinX2 =0;
 var penguinY2 =0;
 
 
-function drawspenguin(){
+function drawPenguin(){
   ctx.clearRect(0,0,canvas.canvas.width, canvas.height);
   ctx.drawImage(image,penguinX, penguinY)
 
 }
 
+function updatePenguin(){
 
 
+  penguinX += penguinX2;
+  penguinY += penguinY2;
 
+  if (penguinX < 0 || penguinX > canvas.width - image.width) {
+    penguinX2 = -penguinX2;
 
+  }
+  if (penguinY < 0 || penguinY > canvas.height - image.height) {
+    penguinY2 = -penguinY2;
+  } 
 
+}
+function gameLoop() {
+  updatePenguin();
+  drawPenguin();
+}
 
-
-
-
-  };
+  }
